49c6932a1e0049761681972519941984f9bc3219
yaroslavDp <bayern744@gmail.com>
2023-06-22 15:55:09 +0300

    createFighterPreview enhanced

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,7 +7,51 @@ export function createFighterPreview(fighter, position) {
         className: `fighter-preview___root ${positionClassName}`
     });
 
-    // todo: show fighter info (image, name, health, etc.)
+
+    function createProperty(keyValue) {
+        let icon = '';
+        const nameElement = createElement({ tagName: 'span', className: 'fighter-preview___property' });
+        switch (keyValue[0]) {
+            case 'health':
+                icon = '❤️';
+                break;
+            case 'attack':
+                icon = '⚔️';
+                break;
+            case 'defense':
+                icon = '🛡️';
+                break;
+            default:
+                break;
+        }
+        nameElement.innerText = keyValue.join(': ').replace(/(\w+):/, icon);
+
+        return nameElement;
+    }
+
+    function createPreviewImage(source) {
+        const attributes = { src: source };
+        const imgElement = createElement({
+            tagName: 'img',
+            className: 'fighter-image___preview',
+            attributes
+        });
+
+        if (position === 'right') {
+            imgElement.style.transform = 'scale(-1, 1)';
+        }
+
+        return imgElement;
+    }
+
+    if (fighter) {
+        fighterElement.append(createPreviewImage(fighter.source));
+        const fightPropsContainer = createElement({ tagName: 'div', className: 'fighter-preview___property-box' });
+        Object.entries(fighter)
+            .filter(keyValueAll => keyValueAll[0] !== '_id' && keyValueAll[0] !== 'source')
+            .forEach(keyValue => fightPropsContainer.append(createProperty(keyValue)));
+        fighterElement.append(fightPropsContainer);
+    }
 
     return fighterElement;
 }
